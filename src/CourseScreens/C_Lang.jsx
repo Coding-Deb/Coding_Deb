101358add84f05ad16313b4c7ebae28497c958db
Coding-Deb <debanshubrahma1234@gmail.com>
2023-11-06 08:44:33 +0530

    new commit

--- src/CourseScreens/C_Lang.jsx
+++ src/CourseScreens/C_Lang.jsx
@@ -12,7 +12,7 @@ export const C_Lang = () => {
     <Row>
     <Col md={6} style={{ backgroundColor: "#F5F5DC", padding: "15px" }}>
       <Container fluid>
-        <h4 className="text-center"><strong>1.</strong> Print Hello World</h4>
+        <h4 className="text-center"><strong>1.</strong> Print Hello World In C Language</h4>
         <SyntaxHighlighter language="c" style={darcula} customStyle={{ fontSize: "16px" }}>
           {'int main() {\n    printf("Hello, World!");\n    return 0;\n}'}
         </SyntaxHighlighter>
@@ -20,13 +20,32 @@ export const C_Lang = () => {
     </Col>
     <Col md={6} style={{ backgroundColor: "#F5F5DC", padding: "15px" }}>
       <Container fluid>
-        <h4 className="text-center">Hello World</h4>
+      <h4 className="text-center"><strong>2.</strong> Basic Variable in C Language</h4>
         <SyntaxHighlighter language="c" style={darcula} customStyle={{ fontSize: "16px" }}>
-          {code}
+          {"int main() {\n   int a= 45; // Here int means integer \n print(a) // 45 \n float a= 22.3; // Here float means floating value (point value) \n print(a) // 22.300 \n char ch = 'A'; // Here In 'A' Sign means character Value \n print(ch); // Here the String Value Ptint , A  \n  return 0;\n}"}
         </SyntaxHighlighter>
       </Container>
     </Col>
     </Row>
+    <Row>
+    <Col md={6} style={{ backgroundColor: "#F5F5DC", padding: "15px" }}>
+      <Container fluid>
+        <h4 className="text-center"><strong>3.</strong>Take User Input And Print It In C Language</h4>
+        <SyntaxHighlighter language="c" style={darcula} customStyle={{ fontSize: "16px" }}>
+          {`int main() {\n int a; // Take a As Interger Variable \n printf('Enter A Value: '); // Just Print A Line To Show How We Take Input \n scanf("%d", &a); // Here The Entered Value Of a Is Stored Using scanf() \n printf("Your number is: %d", a); // Here The Entered Value of a Will Print \n    return 0;\n}`}
+        </SyntaxHighlighter>
+      </Container>
+    </Col>
+    <Col md={6} style={{ backgroundColor: "#F5F5DC", padding: "15px" }}>
+      <Container fluid>
+      <h4 className="text-center"><strong>2.</strong> Basic Variable in C Language</h4>
+        <SyntaxHighlighter language="c" style={darcula} customStyle={{ fontSize: "16px" }}>
+          {"int main() {\n   int a= 45; // Here int means integer \n print(a) // 45 \n float a= 22.3; // Here float means floating value (point value) \n print(a) // 22.300 \n char ch = 'A'; // Here In 'A' Sign means character Value \n print(ch); // Here the String Value Ptint , A  \n  return 0;\n}"}
+        </SyntaxHighlighter>
+      </Container>
+    </Col>
+    </Row>
+    
     </Col>
   );
 };
